306f1bd22c04d579ab5dfec206883d00c880546d
Rena Hashimi <renhas54143@stud.noroff.no>
2025-02-28 14:19:12 +0100

    Changes text size  // style

--- src/Sections/NavBar/index.jsx
+++ src/Sections/NavBar/index.jsx
@@ -5,13 +5,15 @@ function NavBar() {
   const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <header className="fixed flex justify-between items-center top-0 left-0 right-0 z-50 bg-black bg-opacity-90 border-b-2 border-custom-dark text-white sm:tracking-wider w-full p-4 font-play uppercase">
+    <header className="fixed flex justify-between items-center top-0 left-0 right-0 z-50 bg-black bg-opacity-90 border-b-2 border-custom-dark text-white sm:tracking-wider w-full p-4 font-play">
       <div className="flex items-center">
         {/* Logo & Name */}
         <div className="flex items-center">
-          <img src="/logo2.png" alt="Logo" className="w-12 h-auto"/>
+          <img src="/logo2.png" alt="Logo" className="w-10 h-auto"/>
           <a href="#home">
-            <h2 className="text-custom-light font-prata ms-3 mt-2">Rena Hashimi</h2>
+          <h2 className="uppercase text-custom-light font-prata mt-2 ms-3 text-base md:text-xl hover:text-custom-medium transition-all">
+            rena hashimi
+          </h2>          
           </a>
         </div>
       </div>
@@ -33,7 +35,7 @@ function NavBar() {
         origin-top md:origin-center backdrop-blur-md shadow-lg md:shadow-none rounded-md md:rounded-none`}
         aria-label="Main Navigation"
       >
-      <ul className="flex flex-col md:flex-row md:space-x-8 text-center md:text-left p-4 md:p-0">
+      <ul className="flex flex-col md:flex-row md:space-x-8 text-center md:text-left lg:text-xl p-4 md:p-0 uppercase">
         <li><a href="#projects" onClick={() => setIsOpen(false)} className="block py-6 md:py-0 px-2 hover:text-gray-400 transition">Projects</a></li>
         <li><a href="#skills" onClick={() => setIsOpen(false)} className="block py-6 md:py-0 px-2 hover:text-gray-400 transition">Skills</a></li>
         <li><a href="#about" onClick={() => setIsOpen(false)} className="block py-6 md:py-0 px-2 hover:text-gray-400 transition">About Me</a></li>
