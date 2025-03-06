dcf6bbe5457ec4be6a2564436d6a5fc44286170e
Rena Hashimi <renhas54143@stud.noroff.no>
2025-03-06 22:01:22 +0100

    Update index.jsx

--- src/Sections/AboutMe/index.jsx
+++ src/Sections/AboutMe/index.jsx
@@ -28,7 +28,7 @@ function AboutMe() {
         {/* Profile Image */}
         <img
           src="/myImg.jpeg"
-          alt="Rena Hashimi, Front-End Developer"          className="w-48 md:w-60 lg:w-72 h-auto rounded-full opacity-85 object-cover md:mt-[40px]"
+          alt="Rena Hashimi"          className="w-48 md:w-60 lg:w-72 h-auto rounded-full opacity-85 object-cover md:mt-[40px]"
         />
 
         {/* Description Content */}
